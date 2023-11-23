8be42ea29a482775fd1638e853d2997371bcc328
Kerven Kildhery <kervensilvva@gmail.com>
2023-11-23 15:17:21 -0300

    explorando outras formas de listar registros usando o foreach

--- index.php
+++ index.php
@@ -9,21 +9,34 @@
         $conexao = new PDO($dsn, $usuario, $senha);
 
         $query = '
-            select * from tb_usuarios where id = 21
+            select * from tb_usuarios
         ';
 
-        $stmt = $conexao->query($query);
-        $usuario = $stmt->fetch(PDO::FETCH_OBJ);
+        foreach($conexao->query($query) as $key => $value) {
+            print_r($value);
+            //print_r($value['nome']);
+            //print_r($value[1]);
+            echo '<hr>';
 
-/*         para fazer consultas de apenas um dado, temos que alterar primeiramente a query com a regra de consultas
-        em seguida mudar o metodo do stmt para somente fetch (as regras anteriormente com o fetechAll funcionam igualmente) */
+            /* outra forma de fazer consulta por listagem de registro é usando a tecnica 
+            query-one-the-one, onde toda a logica da consulta fica simplemente dentro 
+            de um foreach sem precisar do fetchAll. Por padrão ele vem com os tipos de associação, tanto por indice ou nome 
+            e para alterarmos o tipo de retorno por somente indice ou nome, é só colocar como parâmetro o tipo específico do retorno que queremos*/
+        }  
 
-        echo '<pre>';
-        print_r($usuario);
-        echo '</pre>';
 
-        //echo $usuario[0]['nome'];
-        echo $usuario->nome;
+        //$stmt = $conexao->query($query);
+
+        /* $lista_usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach($lista_usuarios as $key => $value) {
+            echo $value['nome'];
+            echo '<hr>';
+        } */
+
+        /* essa é uma maneira eficaz de fazermos listagem de registros em um banco de dados a partir do comando
+        foreach, conseguimos listar todos os dados de uma coluna de uma tabela, ou todos os dados de uma tabala de forma mais separada. */
+
 
     } catch(PDOException $e){
         echo 'Erro: ' . $e->getCode() . ' Mensagem: ' . $e->getMessage(); 
