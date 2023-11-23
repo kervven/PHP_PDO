7f264920867e6d1091257654c8530e7576308433
Kerven Kildhery <kervensilvva@gmail.com>
2023-11-23 12:59:00 -0300

    tratando exceções de conexão com banco de dados

--- index.php
+++ index.php
@@ -4,4 +4,13 @@
     $usuario = 'root';
     $senha = '';
 
-    $conexao = new PDO($dsn, $usuario, $senha);
+
+    try{
+        $conexao = new PDO($dsn, $usuario, $senha);
+
+    } catch(PDOException $e){
+        echo 'Erro: ' . $e->getCode() . ' Mensagem: ' . $e->getMessage(); 
+        echo '<br> <br>';
+        echo "Esse banco de dados nao existe";
+    }
+
