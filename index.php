6e2e4cbe1be6ec111057b68e5f06bffd2d7519e7
Kerven Kildhery <kervensilvva@gmail.com>
2023-11-23 15:54:01 -0300

    iniciando estudos sobre sql injection

--- index.php
+++ index.php
@@ -1,46 +1,53 @@
 <?php
 
-    $dsn = 'mysql:host=localhost;dbname=php_pdo';
-    $usuario = 'root';
-    $senha = '';
+    if(!empty($_POST['usuario']) && !empty($_POST['senha'])) {
 
+        $dsn = 'mysql:host=localhost;dbname=php_pdo';
+        $usuario = 'root';
+        $senha = '';
 
-    try{
-        $conexao = new PDO($dsn, $usuario, $senha);
+        try{
+            $conexao = new PDO($dsn, $usuario, $senha);
 
-        $query = '
-            select * from tb_usuarios
-        ';
+            $query = "select * from tb_usuarios where ";
+            $query .= " email = '{$_POST['usuario']}' ";
+            $query .= " AND senha = '{$_POST['senha']}' ";
 
-        foreach($conexao->query($query) as $key => $value) {
-            print_r($value);
-            //print_r($value['nome']);
-            //print_r($value[1]);
+            echo $query;
+
+            $stmt = $conexao->query($query);
+            $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
             echo '<hr>';
 
-            /* outra forma de fazer consulta por listagem de registro é usando a tecnica 
-            query-one-the-one, onde toda a logica da consulta fica simplemente dentro 
-            de um foreach sem precisar do fetchAll. Por padrão ele vem com os tipos de associação, tanto por indice ou nome 
-            e para alterarmos o tipo de retorno por somente indice ou nome, é só colocar como parâmetro o tipo específico do retorno que queremos*/
-        }  
+            echo '<pre>';
+            print_r($usuario);
+            echo '</pre>';
+        } catch(PDOException $e){
+            echo 'Erro: ' . $e->getCode() . ' Mensagem: ' . $e->getMessage(); 
+            echo '<br> <br>';
+            echo "Esse banco de dados nao existe";
+        }
+    };
 
 
-        //$stmt = $conexao->query($query);
 
-        /* $lista_usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
 
-        foreach($lista_usuarios as $key => $value) {
-            echo $value['nome'];
-            echo '<hr>';
-        } */
-
-        /* essa é uma maneira eficaz de fazermos listagem de registros em um banco de dados a partir do comando
-        foreach, conseguimos listar todos os dados de uma coluna de uma tabela, ou todos os dados de uma tabala de forma mais separada. */
-
-
-    } catch(PDOException $e){
-        echo 'Erro: ' . $e->getCode() . ' Mensagem: ' . $e->getMessage(); 
-        echo '<br> <br>';
-        echo "Esse banco de dados nao existe";
-    }
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+<body>
+    <form method="post" action="index.php">
+        <input type="text" placeholder="usuário" name="usuario">
+        <br>
+        <input type="password" placeholder="senha" name="senha">
+        <br>
+        <button type="submit">entrar</button>
+    </form>
+</body>
+</html>
 
