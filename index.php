009d78dcec2cc4da1c0af30a3c335685db42966d
Kerven Kildhery <kervensilvva@gmail.com>
2023-11-23 14:35:28 -0300

    estudando mais sobre os tipos de retornos do fetchAll

--- index.php
+++ index.php
@@ -13,21 +13,23 @@
         ';
 
         $stmt = $conexao->query($query);
-        $lista = $stmt->fetchAll();
+        $lista = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+        /* por padrão o retorno do metodo fetchAll é um array de arrays do banco de dados vem com
+        os dois tipo de associação (numero e associativa(o nome da coluna)). Para configurar o tipo
+        da associação do array, devemos por como parâmetro do metodo fetch All o comando PDO::FETCH_NUM
+        para associação numerica ou FETCH_ASSOC para associação com os nomes das colunas da tabela.  */
+
+/*         mas também há o tipo FETCH_OBJ que altera o tipo de retorno do fetchAll, fazendo que ele retorne um array de objetos.
+        quando o retorno é um array de objetos e nao mais um array de arrays, a maneira que fazemos para acessar os objetos do banco de dados muda 
+        invés de ser por exemplo echo $lista[0][nome] vai ficar echo $lista[0]->nome; */
 
         echo '<pre>';
         print_r($lista);
         echo '</pre>';
 
-        echo $lista[0]['nome'];
-
-
-        //o exec é um metodo de execução de queries(SQL) para o banco de dados de retorno limitado,
-        //ele apenas executa a query e retorna a quantidade de linhas que foram alteradas.
-        //Para criação de tabelas, por exemplo, o retorno sempre será 0, pois criação de tabela
-        //nao afeta nenhuma linha, mas o seeding no banco de dados sim. É aí que o exec pode ser útil
-        //porque sempre será retornado a quantidade de linhas que aquela determinada query afetou.
-
+        //echo $lista[0]['nome'];
+        echo $lista[0]->nome;
 
     } catch(PDOException $e){
         echo 'Erro: ' . $e->getCode() . ' Mensagem: ' . $e->getMessage(); 
